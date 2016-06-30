d4a82f669311e5aa55c686e8ce97b6a873da541f
Jonas Liekens <jonas.liekens@brickbit.be>
2016-06-30 14:59:51 +0200

    fixed nullpointerex

--- src/main/java/be/brickbit/lpm/core/service/user/mapper/UserDetailsDtoMapper.java
+++ src/main/java/be/brickbit/lpm/core/service/user/mapper/UserDetailsDtoMapper.java
@@ -2,6 +2,7 @@ package be.brickbit.lpm.core.service.user.mapper;
 
 import be.brickbit.lpm.core.domain.User;
 import be.brickbit.lpm.core.service.user.dto.UserDetailsDto;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -11,14 +12,18 @@ import java.time.temporal.ChronoUnit;
 public class UserDetailsDtoMapper implements UserMapper<UserDetailsDto> {
     @Override
     public UserDetailsDto map(User user) {
-        return new UserDetailsDto(
-                user.getUsername(),
-                user.getBirthDate().until(LocalDate.now(), ChronoUnit.YEARS),
-                user.getWallet(),
-                user.getMood(),
-                user.getFirstName(),
-                user.getLastName(),
-                user.getEmail()
-        );
+        if(user != null) {
+            return new UserDetailsDto(
+                    user.getUsername(),
+                    user.getBirthDate().until(LocalDate.now(), ChronoUnit.YEARS),
+                    user.getWallet(),
+                    user.getMood(),
+                    user.getFirstName(),
+                    user.getLastName(),
+                    user.getEmail()
+            );
+        }else{
+            throw new UsernameNotFoundException("User not found.");
+        }
     }
 }
