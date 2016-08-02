022cdf5ef8a9e036a918e8a65ea7e164384b73a1
Jonas Liekens <jonas.liekens@brickbit.be>
2016-08-02 20:38:55 +0200

    -- Simplified AbstractIT

--- src/test/java/be/brickbit/lpm/core/AbstractIT.java
+++ src/test/java/be/brickbit/lpm/core/AbstractIT.java
@@ -1,11 +1,6 @@
 package be.brickbit.lpm.core;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,46 +11,10 @@ public abstract class AbstractIT {
 	private EntityManager entityManager;
 
 	protected void insert(Object... entities) throws Exception {
-		List<Object> entitiesToSave = new ArrayList<>();
-
 		for (Object entity : entities) {
-			entitiesToSave.addAll(getDependencies(entity));
-		}
-
-		Collections.reverse(entitiesToSave);
-
-		for (Object entity : entitiesToSave) {
 			save(entity);
 		}
 	}
-
-	private List<Object> getDependencies(Object entity) throws IllegalAccessException {
-        List<Object> entities = new ArrayList<>();
-        entities.add(entity);
-
-        for (Field field : entity.getClass().getDeclaredFields()) {
-            if (field.isAnnotationPresent(OneToOne.class) ||
-                    field.isAnnotationPresent(ManyToOne.class)) {
-
-                field.setAccessible(true);
-                Object dependency = field.get(entity);
-
-                if (dependency != null) {
-                    entities.addAll(getDependencies(field.get(entity)));
-                }
-            } else if (field.isAnnotationPresent(ManyToMany.class) ||
-                    field.isAnnotationPresent(OneToMany.class)) {
-                field.setAccessible(true);
-                Iterable dependencies = (Iterable) field.get(entity);
-
-                for (Object dependency : dependencies) {
-                    entities.addAll(getDependencies(dependency));
-                }
-            }
-        }
-
-        return entities;
-    }
 
 	private void save(Object object) {
 		if (entityManager.contains(object)) {
