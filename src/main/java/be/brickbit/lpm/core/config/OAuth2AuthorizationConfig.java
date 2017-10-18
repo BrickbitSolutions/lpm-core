d84b3af1d4ffb5f7a7dafe84c35de4e007e606b1
Jonas Liekens <jonas.liekens@brickbit.be>
2017-10-18 15:41:29 +0200

    Enabled user role vs scope check + added OUT folder to .gitignore

--- src/main/java/be/brickbit/lpm/core/config/OAuth2AuthorizationConfig.java
+++ src/main/java/be/brickbit/lpm/core/config/OAuth2AuthorizationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
@@ -20,17 +21,22 @@ import be.brickbit.lpm.core.service.api.user.UserService;
 @EnableAuthorizationServer
 public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final DataSource dataSource;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private DataSource dataSource;
+    public OAuth2AuthorizationConfig(
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder,
+            UserService userService,
+            DataSource dataSource) {
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public JdbcTokenStore tokenStore() {
@@ -39,8 +45,12 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        DefaultOAuth2RequestFactory requestFactory = new DefaultOAuth2RequestFactory(endpoints.getClientDetailsService());
+        requestFactory.setCheckUserScopes(true);
+
         endpoints
                 .authenticationManager(authenticationManager)
+                .requestFactory(requestFactory)
                 .userDetailsService(userService)
                 .tokenStore(tokenStore());
     }
